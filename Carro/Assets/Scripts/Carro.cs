ce4d985f7e1daf7719420949b86d522f65ef4497
Kimonaro <g.kira@escolar.ifrn.edu.br>
2026-01-26 14:35:59 -0300

    script carro
    39:23 video

---
+++ Carro/Assets/Scripts/Carro.cs
@@ -0,0 +1,52 @@
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Carro : MonoBehaviour
+{
+    public float velocidadeMaxima = 10f;
+
+    public ConstantForce2D forca;
+    
+    public bool capotado = false;
+
+    public enum Direcao
+    {
+        Frente, Traz
+    }
+
+    public Direcao direcao = Direcao.Frente;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direcao = Direcao.Frente;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direcao = Direcao.Traz;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (direcao == Direcao.Frente)
+            {
+                forca.force = Vector2.right * velocidadeMaxima;
+            }
+            else if (direcao == Direcao.Traz)
+            {
+                forca.force = Vector2.left * velocidadeMaxima;
+            }
+            
+        }
+        else
+        {
+            forca.force = Vector2.zero;
+        }
+    }
+}
